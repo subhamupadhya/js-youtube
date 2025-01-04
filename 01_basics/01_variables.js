ab9fc6252ba7f6d27924479726e07ff4297d0091
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-04 05:20:51 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "subhamupadhya53100@gmail.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+
+
+// accountId = 2    // not allowed
+
+accountEmail = "hc@hc.com"
+accountpassword = "232425"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountpassword, accountCity])
