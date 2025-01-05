644a384043d6f85453a7f92c6c3784cbb4e89875
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-05 21:28:31 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" =  33
+// "33abc" = nan
+// true = 1; false = 0
+
+//let isLoggedIn = " "
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+// 1 = true ,  0 = false
+// "" = false
+// "subham " = true
+
+// conversion into string :- 
+let someNumber = 33;
+
+let stringNumber = String(someNumber); 
+console.log(stringNumber); 
+console.log(typeof stringNumber); 
