33e282bf6a5ab236a4f59efa8f4c2d0fee873a45
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-05 22:44:21 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// comparison
+console.log( 2 > 1);
+console.log(2<= 1);
+console.log (2==1);                          // basics
+
+console.log("2" > 1);
+console.log("02" > 1);                    // in compariosn try to do same dataypes that same concept are in typescript are available 
+
+console.log(null > 0); 
+console.log(null < 0);
+console.log(null == 0);
+console.log(null <= 0);           // compariosn convert null to a number as a zero 
+
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+// === (strictly check)
+console.log("2" === 2);
