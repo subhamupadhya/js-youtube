6860d14f68d961984617021e42f873ab2fdbcbe2
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-07 22:47:27 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,5 @@
+const score = 400
+console.log(score);                         // directly declare a value 
+
+const balance = new Number(100)
+console.log(balance);                         // definetly print into number
