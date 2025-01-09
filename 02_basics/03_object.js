310df06697740cbd5c8d379d0ff0608f84b10f6c
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-09 22:17:30 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,5 @@
+// to declare objects mainly two ways : 1.literals  2.constructor
+// with the constructor always made singelton not through literals
+
+// object literals:  way to declare objects 
+
