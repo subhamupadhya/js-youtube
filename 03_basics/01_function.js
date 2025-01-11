78db4e1efa2dd5356cbf648b85401e6df6ce895c
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-11 21:04:17 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,20 @@
+// function:- 
+// to print so many packages in one box known as function
+// when we made function defination and take an input value that is known as parameter
+// when we call the function and passes the value that is known as arguments
+
+function sayMyName(){
+    console.log("S");
+    console.log("U");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("M");
+}
+//sayMyName()
+
+// add  two function
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(3, 4)
