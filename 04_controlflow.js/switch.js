49a692c8b4e3c8f791f194e2bcc899a1b8b1c0bf
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-14 19:59:21 +0000

    switch

---
+++ 04_controlflow.js/switch.js
@@ -0,0 +1,30 @@
+/* switch statement
+
+switch (key) {
+case value:
+
+break;
+
+default:
+    break;
+} */
+
+    const month = 3
+    switch (month) {
+        case 1:
+            console.log("january");                   // shift + alt + down arrow => same clone
+            break;
+        case 2:
+            console.log("february");
+            break;
+        case 3:
+            console.log("march");
+            break;
+        case 2:
+            console.log("april");
+            break;
+    
+        default:
+            console.log("default case match")
+            break;
+    }
