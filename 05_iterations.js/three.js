6df46af1278ffe7cc007a7a9cf8c30a4bc1aca7d
Subham Kumar <157126041+subhamupadhya@users.noreply.github.com>
2025-01-16 21:17:07 +0000

    loops

--- 05_iterations.js/three.js
+++ 05_iterations.js/three.js
@@ -8,7 +8,7 @@ const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
     console.log(num);
-}
+} 
 
 
 const greetings = "hello world!"
@@ -17,7 +17,8 @@ for (const greet of greetings) {
 }
     
 // Maps
-// the map object holds key-values pairs and remembers the originl insertion order of the keys
+// the map object holds key-values pairs and remembers the originl 
+// insertion order of the keys and it also follow unique values
 const map = new Map()
 map.set('IN', "India")
 map.set('Gr', "Germany")
@@ -27,8 +28,18 @@ map.set('IN', "India")
 console.log(map);
 
 
+for (const key of map) {
+    console.log(key);
+}
 
-
+// four loop in object
+const myObject = {
+    'gane1' : 'NFS',
+    'game2' : 'spiderman'
+}
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+}
 
 
 
